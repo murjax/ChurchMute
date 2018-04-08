09ba0565993aa2447afbcae1f43dffe84a6ea16a
Ryan Murphy <murjax@gmail.com>
2018-04-08 18:09:08 -0400

    Mute device when current location is a church

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import CheckBox from 'react-native-checkbox';
 import SystemSetting from 'react-native-system-setting'
+import RNGooglePlaces from 'react-native-google-places'
 
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props)
+    this.state = {
+      placeResults: []
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -23,13 +30,28 @@ export default class App extends Component<Props> {
             label='School'
           />
           <Button title="Mute Me" color="#4286f4" onPress={() => this.mute()} />
+          <Button title="Search" color="#4286f4" onPress={() => this.getCurrentPlace()} />
         </View>
       </View>
     );
   }
 
+  getCurrentPlace(){
+    RNGooglePlaces.getCurrentPlace()
+      .then((results) => {
+        results.map((result) =>{
+          result.types.map((type) =>{
+            console.log(type)
+            if(type == 'church'){
+              this.mute()
+            }
+          })
+        })
+      })
+      .catch((error) => console.log(error.message));
+  }
+
   mute(){
-    // SystemSetting.setVolume(0.5);
     SystemSetting.setVolume(0, {type: 'ring', playSound: false, showUI: false});
     SystemSetting.getVolume('ring').then((volume)=>{
       console.log('Current volume is ' + volume);
