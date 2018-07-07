ad10c8e7b235a38fbbfc23f838a06e32a9f02a44
Ryan Murphy <murjax@gmail.com>
2018-07-07 19:08:09 -0400

    Extract option logic into component

--- App.js
+++ App.js
@@ -4,48 +4,14 @@ import { globalStyles } from './styles/global_styles.js';
 import CheckBox from 'react-native-checkbox';
 import SystemSetting from 'react-native-system-setting'
 import RNGooglePlaces from 'react-native-google-places'
+import Options from './components/Options.js'
 
 export default class App extends Component<Props> {
-  constructor(props){
-    super(props)
-    this.state = {
-      placeResults: []
-    }
-  }
   render() {
     return (
       <View style={globalStyles.container}>
-        <View style={globalStyles.heading}>
-          <Text style={globalStyles.title}>ChurchMuter</Text>
-        </View>
-
-        <View style={globalStyles.form}>
-          <Button title="Mute Me" color="#4286f4" onPress={() => this.mute()} />
-          <Button title="Search" color="#4286f4" onPress={() => this.getCurrentPlace()} />
-        </View>
+        <Options />
       </View>
     );
-  }
-
-  getCurrentPlace(){
-    RNGooglePlaces.getCurrentPlace()
-      .then((results) => {
-        results.map((result) =>{
-          result.types.map((type) =>{
-            console.log(type)
-            if(type == 'church'){
-              this.mute()
-            }
-          })
-        })
-      })
-      .catch((error) => console.log(error.message));
-  }
-
-  mute(){
-    SystemSetting.setVolume(0, {type: 'ring', playSound: false, showUI: false});
-    SystemSetting.getVolume('ring').then((volume)=>{
-      console.log('Current volume is ' + volume);
-    });
   }
 }
---
+++ components/Options.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import CheckBox from 'react-native-checkbox';
+import SystemSetting from 'react-native-system-setting'
+import RNGooglePlaces from 'react-native-google-places'
+import { globalStyles } from '../styles/global_styles.js';
+
+export default class extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      placeResults: []
+    }
+  }
+  render() {
+    return (
+      <View>
+        <View style={globalStyles.heading}>
+          <Text style={globalStyles.title}>ChurchMuter</Text>
+        </View>
+
+        <View style={globalStyles.form}>
+          <Button title="Mute Me" color="#4286f4" onPress={() => this.mute()} />
+          <Button title="Search" color="#4286f4" onPress={() => this.getCurrentPlace()} />
+        </View>
+      </View>
+    );
+  }
+
+  getCurrentPlace(){
+    RNGooglePlaces.getCurrentPlace()
+      .then((results) => {
+        results.map((result) =>{
+          result.types.map((type) =>{
+            console.log(type)
+            if(type == 'church'){
+              this.mute()
+            }
+          })
+        })
+      })
+      .catch((error) => console.log(error.message));
+  }
+
+  mute(){
+    SystemSetting.setVolume(0, {type: 'ring', playSound: false, showUI: false});
+    SystemSetting.getVolume('ring').then((volume)=>{
+      console.log('Current volume is ' + volume);
+    });
+  }
+}
